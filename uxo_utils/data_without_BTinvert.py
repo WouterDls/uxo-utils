e8fed0a8ca2aa409b22db38eae6530a31c5b8749
Wouter Deleersnyder <wouter.deleersnyder@student.kuleuven.be>
2026-03-04 16:13:33 -0800

    Create data_without_BTinvert.py

---
+++ uxo_utils/data_without_BTinvert.py
@@ -0,0 +1,389 @@
+import h5py
+import os
+import numpy as np
+from sklearn.linear_model import LinearRegression
+import warnings
+
+from .imports import data_dir, main_data_dir, DoD_ordnance_dir, sensor_def_dir
+from .parse import proc_group
+
+def load_ordnance_dict(
+    directory=DoD_ordnance_dir,
+    filenames=[
+        "ordnance_DoD_UltraTEM_5F_APG.h5",
+        "ordnance_DoD_UltraTEM_5F_ISOsmall.h5",
+        "ordnance_DoD_UltraTEM_NATA_dyn_F_scale0o86.h5"
+    ]
+):
+    """
+    create a dictionary of ordnance object from h5 files
+    """
+    ord_dict = {}
+
+    for file in filenames:
+        ord_file = os.path.join(directory, file)
+        f = h5py.File(ord_file, 'r')
+        for i in f['ordnance']:
+            ord_name = str(f[f'ordnance/{i}/Name'][()][0]).split("'")[1]
+
+            L3 = f[f'ordnance/{i}/L1ref'][()].flatten()
+            L2 = f[f'ordnance/{i}/L2ref'][()].flatten()
+            L1 = f[f'ordnance/{i}/L3ref'][()].flatten()
+            size_mm = int(f[f'ordnance/{i}/size_mm'][()].flatten())
+            common_name = f[f'ordnance/{i}/h5_Common_Name'][()].flatten()[0]
+
+            if isinstance(common_name, list):
+                common_name = common_name[0]
+
+            if ord_name not in ord_dict.keys():
+                times = f[f'ordnance/{i}/time'][()].flatten()
+                ord_dict[ord_name] = {
+                    "L3": [L3],
+                    "L2": [L2],
+                    "L1": [L2],
+                    "size mm": [size_mm],
+                    "common name": [common_name],
+                    "times": times,
+                }
+            else:
+                for key, val in zip(
+                    ["L3", "L2", "L1", "size mm", "common name"],
+                    [L3, L2, L1, size_mm, common_name]
+                ):
+                    ord_dict[ord_name][key].append(val)
+
+    return ord_dict
+
+
+
+def load_h5_data(filepath):
+
+    # load into a dict with h5py
+    dfile = os.path.join(filepath)
+    f = h5py.File(dfile, 'r')
+    dictionary = proc_group(f)
+    f.close()
+
+    return dictionary
+
+def rotate_survey(local_x, local_y):
+    model = LinearRegression(fit_intercept=False)
+    model.fit(local_x[:, np.newaxis], local_y)
+    slope = model.coef_[0]
+    theta = np.arctan(slope) + np.pi/2
+    rotated_x = np.cos(theta) * local_x + np.sin(theta) * local_y
+    rotated_y = -np.sin(theta) * local_x + np.cos(theta) * local_y
+    return theta, slope, rotated_x, rotated_y
+
+def create_survey_from_file(filepath, convert_degrees_to_radians=False):
+    dic = load_h5_data(filepath)
+
+    xyz_dict = dic["XYZ"]
+    xyz_data = xyz_dict["Data"]
+
+    times = np.array(dic['SensorTimes'].flatten())
+
+    def get_index(key):
+        if key in xyz_dict["Info"].keys():
+            return xyz_dict["Info"][key]["ChannelIndex"].flatten().astype(int)-1
+        else:
+            if type(dic['XYZ']['Info']['ChannelNames']) == str:
+                text = dic['XYZ']['Info']['ChannelNames']
+                l = np.array([item.strip() for item in text.split(",")])
+            else:
+                l = dic['XYZ']['Info']['ChannelNames']
+            # Text appears as long string with comma-separated values. We need to split and strip it to get the individual channel names.
+            return np.where(l == str(key))[0] # no -1 needed, as pythonic approach
+
+    def get_values(key):
+        index = get_index(key)
+        return xyz_data[index, :].flatten()
+
+    easting = get_values("Easting")
+    northing = get_values("Northing")
+
+    yaw = get_values("Yaw")
+    pitch = get_values("Pitch")
+    roll = get_values("Roll")
+
+    if convert_degrees_to_radians:
+        yaw = yaw*np.pi/180
+        pitch = pitch*np.pi/180
+        roll = roll*np.pi/180
+    else:
+        if not all(np.abs(yaw) < 2*np.pi):
+            warnings.warn("There are yaw values above 2 pi. You might need to convert to radians `convert_degrees_to_radians=True`")
+
+    mnum = get_values("MeasNum").astype(int) - 1 # index from zero in python
+    line = get_values("Line").astype(int)
+    rx_num = get_values("RxNum").astype(int) - 1
+    tx_num = get_values("TxNum").astype(int) - 1
+    rx_comp = get_values("RxCNum").astype(int) - 1
+    data = xyz_data[get_index("Data"), :].T # Filtered data
+    # Extension w.r.t. 2021-uxo-utils
+    if 'tearly_filterdist' in xyz_dict['Info']['ChannelNames']:
+        tearly = get_values("tearly")
+        tearly_filterdist = get_values("tearly_filterdist")
+        tmid = get_values("tmid")
+        tmid_filterdist = get_values("tmid_filterdist")
+        tlate = get_values("tlate")
+        tlate_filterdist = get_values("tlate_filterdist")
+        elevation = get_values("Elevation")
+        # Extracting raw data (unfiltered). It assumes that the indices are "logically"-ordered between the early and late times.
+        if type(dic['XYZ']['Info']['ChannelNames']) == str:
+            text = dic['XYZ']['Info']['ChannelNames']
+            ChannelNames = np.array([item.strip() for item in text.split(",")])
+        else:
+            ChannelNames = dic['XYZ']['Info']['ChannelNames']
+        if times.size == 27:
+            idx0 = np.where(ChannelNames== str('t154'))[0]
+            idxn = np.where(ChannelNames == str('t2420'))[0]
+
+        elif times.size == 25:
+            idx0 = np.where(ChannelNames== str('t190'))[0][0]
+            idxn = np.where(ChannelNames == str('t2420'))[0][0]
+
+        else:
+            raise ValueError("Unexpected number of time channels. Cannot determine index for raw data.")
+        try:
+            raw_data = xyz_data[idx0:idxn+1, :].T
+        except:
+            raw_data = None
+            warnings.warn("Could not extract raw data. Check channel names and indices.")
+        return Survey_from_h5(times, easting, northing, pitch, roll, yaw, mnum, data, line, rx_num, tx_num, rx_comp,
+                              tearly, tearly_filterdist, tmid, tmid_filterdist, tlate, tlate_filterdist, elevation, raw_data)
+    else:
+        return Survey(times, easting, northing, pitch, roll, yaw, mnum, data, line, rx_num, tx_num, rx_comp)
+
+
+class Survey:
+    """
+    Minimal class to hold survey information and derived properties.
+    """
+    def __init__(
+        self,
+        times,
+        easting,
+        northing,
+        pitch,
+        roll,
+        yaw,
+        mnum,
+        data=None,
+        line=None,
+        rx_num=None,
+        tx_num=None,
+        rx_comp=None,
+    ):
+        self._data = data
+        self._time = times
+        self._easting = easting
+        self._northing = northing
+        self._pitch = pitch
+        self._roll = roll
+        self._yaw = yaw
+        self._mnum = mnum
+        self._line = line
+        self._rx_num = rx_num
+        self._tx_num = tx_num
+        self._rx_comp = rx_comp
+
+
+    # Properties directly from data file
+    @property
+    def times(self):
+        """
+        Time values for each time channel.
+        """
+        return self._times
+
+    @property
+    def easting(self):
+        return self._easting
+
+    @property
+    def northing(self):
+        return self._northing
+
+    @property
+    def yaw(self):
+        return self._yaw
+
+    @property
+    def pitch(self):
+        return self._pitch
+
+    @property
+    def roll(self):
+        return self._roll
+
+    @property
+    def mnum(self):
+        """
+        Measurement number for each measurement. It corresponds to a unique receiver-transmitter (incl. orientation) pair.
+        """
+        return self._mnum
+
+    @property
+    def line(self):
+        """
+        Line number for EACH measurement. Length matches number of measurements.
+        """
+        return self._line
+
+    @property
+    def rx_num(self):
+        """
+        Receiver number. Length matches number of measurements.
+        """
+        return self._rx_num
+
+    @property
+    def tx_num(self):
+        """
+        Transmitter number. Length matches number of measurements.
+        """
+        return self._tx_num
+
+    @property
+    def data(self):
+        """
+        FILTERED dB/dt data. Shape is (number of measurements, number of time channels).
+        """
+        return self._data
+
+    # Derived properties
+    @property
+    def x0(self):
+        if getattr(self, "_x0", None) is None:
+            self._x0 = np.r_[self.easting.mean(), self.northing.mean()]
+        return self._x0
+
+    @property
+    def local_x(self):
+        if getattr(self, "_local_x", None) is None:
+            self._local_x = self.easting - self.x0[0]
+        return self._local_x
+
+    @property
+    def local_y(self):
+        if getattr(self, "_local_y", None) is None:
+            self._local_y = self.northing - self.x0[1]
+        return self._local_y
+
+    def _rotate_survey(self):
+        """
+        Rotate survey coordinates to align with survey direction.
+        """
+        theta, slope, rotated_x, rotated_y = rotate_survey(self.local_x, self.local_y)
+        self._theta = theta
+        self._slope = slope
+        self._rotated_x = rotated_x
+        self._rotated_y = rotated_y
+        self._rotated_yaw = self.yaw + theta
+
+    @property
+    def slope(self):
+        if getattr(self, "_slope", None) is None:
+            self._rotate_survey()
+        return self._slope
+
+    @property
+    def rotated_x(self):
+        if getattr(self, "_rotated_x", None) is None:
+            self._rotate_survey()
+        return self._rotated_x
+
+    @property
+    def rotated_y(self):
+        if getattr(self, "_rotated_y", None) is None:
+            self._rotate_survey()
+        return self._rotated_y
+
+    @property
+    def rotated_yaw(self):
+        if getattr(self, "_rotated_y", None) is None:
+            self._rotate_survey()
+        return self._rotated_yaw
+
+    @property
+    def unique_lines(self):
+        """
+        Unique line numbers in the survey.
+        """
+        if getattr(self, "_unique_lines", None) is None:
+            self._unique_lines = np.unique(self.line)
+        return self._unique_lines
+
+
+class Survey_from_h5(Survey):
+    def __init__(self,
+                times,
+                easting,
+                northing,
+                pitch,
+                roll,
+                yaw,
+                mnum,
+                data,
+                line,
+                rx_num,
+                tx_num,
+                rx_comp,
+                tearly=None,
+                tearly_filterdist=None,
+                tmid=None,
+                tmid_filterdist=None,
+                tlate=None,
+                tlate_filterdist=None,
+                elevation=None,
+                raw_data=None):
+
+        super().__init__(times, easting, northing, pitch, roll, yaw, mnum, data, line, rx_num, tx_num, rx_comp)
+        self._tearly = tearly
+        self._tearly_filterdist = tearly_filterdist
+        self._tmid = tmid
+        self._tmid_filterdist = tmid_filterdist
+        self._tlate = tlate
+        self._tlate_filterdist = tlate_filterdist
+        self._elevation = elevation
+        self._raw_data = raw_data
+
+    @property
+    def tearly(self):
+        return self._tearly
+
+    @property
+    def tearly_filterdist(self):
+        return self._tearly_filterdist
+
+    @property
+    def tmid(self):
+        return self._tmid
+
+    @property
+    def tmid_filterdist(self):
+        return self._tmid_filterdist
+
+    @property
+    def tlate(self):
+        return self._tlate
+
+    @property
+    def tlate_filterdist(self):
+        return self._tlate_filterdist
+
+    @property
+    def elevation(self):
+        return self._elevation
+
+    @property
+    def raw_data(self):
+        """
+        RAW (i.e. UNFILTERED) dB/dt data. Shape is (number of measurements, number of time channels).
+        """
+        return self._raw_data
+
+
+    
+
